c2d533af1d2de6cafa202b7d415b5e24da83849c
Your Pavel <p9l-real-chel@mail.ru>
2023-03-14 17:14:23 +0500

    Work 23

---
+++ work23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+try
+{
+Console.Write("Введите N: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumbKub(numb));
+}
+catch
+{
+    Console.WriteLine("Ввели неправильные данные");
+}
+string NumbKub(int numb, string res = "1")
+{
+    if(numb <= 0)
+    {
+        return "Вы ввели неправильное число!";
+    }
+
+    int i = 2;    
+    while(i <= numb)
+    {
+        res =  res + "," + Convert.ToString(i*i*i);
+        i++;
+    }
+    return res;
+}
